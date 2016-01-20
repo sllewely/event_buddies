6472145324a148caadd1270344310bc4849fb65b
Sarah Llewelyn <sllewelyn@mdsol.com>
2016-01-19 19:24:08 -0500

    annotate

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,17 @@
 class Event < ActiveRecord::Base
 
 end
+
+# == Schema Information
+#
+# Table name: events
+#
+#  id          :integer         not null, primary key
+#  name        :string          not null
+#  date        :date            not null
+#  start_time  :time
+#  created_at  :datetime        not null
+#  updated_at  :datetime        not null
+#  description :text
+#
+
