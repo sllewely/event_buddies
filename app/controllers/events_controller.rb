1498940823b26b9c3f930a923250af485277ca05
Sarah Llewelyn <sllewelyn@mdsol.com>
2016-01-19 21:00:49 -0500

    list events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,6 +14,7 @@ class EventsController < ApplicationController
 
   private
 
+  # Strong params for security
   def event_params
     params.require(:event).permit(:name, :description, :date, :start_time)
   end
