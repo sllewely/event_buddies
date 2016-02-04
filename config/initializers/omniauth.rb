9a48c6ef9b01752b400739dfe877b1048a2793c7
fpcyan <fconn@mdsol.com>
2016-02-03 20:42:24 -0500

    Ignore where I put my secret keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,11 @@
+<<<<<<< 37c0bb968c828cfc9e6494ef24b5361c1aca0392
 require 'omniauth-facebook'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
     scope: 'email, user_birthday, public_profile'
+=======
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+>>>>>>> Ignore where I put my secret keys
 end
