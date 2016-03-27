95a533f9519ba43816c0ee4cbf5da5fae398d553
fpcyan <fconn@mdsol.com>
2016-03-27 16:13:13 -0400

    migrate users table

--- db/migrate/20160327200440_create_users.rb
+++ db/migrate/20160327200440_create_users.rb
@@ -2,13 +2,14 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :access_token
-      t.string :string
-      t.string :temp_token
-      t.string :email
+      t.string :temp_token, null: false
+      t.string :email, null: false, index: true
       t.datetime :birthday
-      t.string :first_name
-      t.string :last_name
-      t.string :uuid
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :facebook_uid, null: false
+      t.string :location
+      t.string :uuid, null: false
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160121005359) do
+ActiveRecord::Schema.define(version: 20160327200440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,22 @@ ActiveRecord::Schema.define(version: 20160121005359) do
     t.datetime "updated_at",  null: false
     t.text     "description"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "access_token"
+    t.string   "temp_token",   null: false
+    t.string   "email",        null: false
+    t.datetime "birthday"
+    t.string   "first_name",   null: false
+    t.string   "last_name",    null: false
+    t.string   "facebook_uid", null: false
+    t.string   "location"
+    t.string   "uuid",         null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", using: :btree
 
   create_table "venues", force: :cascade do |t|
     t.string "name"
