5e5cc22363393da61edbf9ef3996b48bb31e53b0
fpcyan <fconn@mdsol.com>
2016-03-27 15:45:46 -0400

    Add files

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,6 @@
+require 'omniauth-facebook'
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+    scope: 'email, user_birthday, public_profile'
+end
