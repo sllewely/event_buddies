11eab98bf76493bf479075135b89cfc68d366b15
Sarah Llewelyn <sllewelyn@mdsol.com>
2016-08-28 12:20:00 -0400

    omniauth facebook gem - omniauth with fb strategy

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,6 @@ require 'omniauth-facebook'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-    scope: 'email, user_birthday, public_profile'
+    scope: 'email, user_birthday, public_profile',
+    display: 'popup'
 end
