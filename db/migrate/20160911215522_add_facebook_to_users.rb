14fa6e150e72a609a9939413a35e869ffd95a5e8
Sarah Llewelyn <sllewelyn@renttherunway.com>
2017-07-25 07:03:23 -0400

    drop auth for now

--- db/migrate/20160911215522_add_facebook_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddFacebookToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :facebook_image, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160911215522) do
+ActiveRecord::Schema.define(version: 20160505000633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,8 +49,6 @@ ActiveRecord::Schema.define(version: 20160911215522) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "access_token"
-    t.string "temp_token", null: false
     t.string "email", null: false
     t.datetime "birthday"
     t.string "first_name", null: false
@@ -59,9 +57,6 @@ ActiveRecord::Schema.define(version: 20160911215522) do
     t.string "uuid", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "provider"
-    t.string "uid"
-    t.string "facebook_image"
     t.index ["email"], name: "index_users_on_email"
   end
 
