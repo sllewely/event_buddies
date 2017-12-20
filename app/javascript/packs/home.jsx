2d014c6d821bdde66648187f4578785694dd1dc7
Sarah Llewelyn <sllewelyn@renttherunway.com>
2017-12-19 23:49:12 -0500

    semantic ui homepage template kindaaa working

--- app/javascript/packs/home.jsx
+++ app/javascript/packs/home.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import { Button } from 'semantic-ui-react'
+import HomepageLayout from './homepage_layout'
 
 const ButtonExampleButton = () => (
     <Button>Click Here</Button>
 )
 
+const HomepageLayoutThing = () => (
+    <HomepageLayout/>
+)
+
 document.addEventListener('DOMContentLoaded', () => {
     ReactDOM.render(
-        <ButtonExampleButton name="React" />,
+        <HomepageLayout />,
         document.body.appendChild(document.createElement('div')),
     )
 })
