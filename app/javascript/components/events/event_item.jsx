c6fb707d88cd989b893398bfb1811b424056bfb3
Happler <harry.appler@gmail.com>
2018-07-15 15:43:32 -0400

    switch div style for expanding

--- app/javascript/components/events/event_item.jsx
+++ app/javascript/components/events/event_item.jsx
@@ -33,50 +33,50 @@ class EventItem extends React.Component {
   render() {
     const { event } = this.props;
     const visibility = this.state.expanded
-      ? "event__item_expanded default_text"
+      ? "event__item_expanded V_Flex"
       : "event__item_hidden";
     return (
       <article className="event__item V_Flex">
-        <section className="H_Flex">
-          <div className="V_Flex">
-            <div className="H_Flex">
-              <h1 className="header_text" onClick={this.toggleExpandedInfo()}>
-                {event.name}
-              </h1>
-              <span
-                className="event__item_spacer"
-                onClick={this.toggleExpandedInfo()}
-              >
-                @
-              </span>
-              <a
-                href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
-                  event.location
-                )}`}
-                className="header_text"
-                target="_blank"
-              >
-                {this.truncate(event.location)}
-              </a>
-            </div>
-            <a href={event.tickets} target="_blank" className={visibility}>
-              Buy Tickets here
+        <div className="H_Flex">
+          <div className="H_Flex">
+            <h1 className="header_text" onClick={this.toggleExpandedInfo()}>
+              {event.name}
+            </h1>
+            <span
+              className="event__item_spacer"
+              onClick={this.toggleExpandedInfo()}
+            >
+              @
+            </span>
+            <a
+              href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+                event.location
+              )}`}
+              className="header_text"
+              target="_blank"
+            >
+              {this.truncate(event.location)}
             </a>
           </div>
-          <div className="V_Flex">
-            <div className="H_Flex">
-              <h1 className="default_text">{event.date}</h1>
-              <i
-                className="material-icons md-36 md-dark event__item_toggle"
-                onClick={this.toggleExpandedInfo()}
-              >
-                {this.state.expanded ? "expand_less" : "expand_more"}
-              </i>
-            </div>
-            <h1 className={visibility}>{event.time}</h1>
+          <div className="H_Flex">
+            <h1 className="default_text">{event.date}</h1>
+            <i
+              className="material-icons md-36 md-dark event__item_toggle"
+              onClick={this.toggleExpandedInfo()}
+            >
+              {this.state.expanded ? "expand_less" : "expand_more"}
+            </i>
           </div>
+        </div>
+        <section className={visibility}>
+          <div className="H_Flex">
+            <a href={event.tickets} target="_blank" className="default_text">
+              Buy Tickets here
+            </a>
+            <h1 className="default_text">{event.time}</h1>
+          </div>
+          <p className="default_text">{event.description}</p>
         </section>
-        <p className={visibility}>{event.description}</p>
       </article>
     );
   }
