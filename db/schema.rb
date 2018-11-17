8df220ca6cf1d45e81ef83f26ee9d16cf9c24d9a
Sarah Llewelyn <se.llewelyn@gmail.com>
2018-11-17 15:05:51 -0500

    gitignore and stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_30_182848) do
+ActiveRecord::Schema.define(version: 2018_10_05_021154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
 
   create_table "event_statuses", force: :cascade do |t|
     t.bigint "event_id", null: false
@@ -34,7 +35,9 @@ ActiveRecord::Schema.define(version: 2018_09_30_182848) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
+    t.uuid "uuid", default: -> { "uuid_generate_v4()" }, null: false
     t.index ["user_id"], name: "index_events_on_user_id"
+    t.index ["uuid"], name: "index_events_on_uuid", unique: true
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,8 +55,10 @@ ActiveRecord::Schema.define(version: 2018_09_30_182848) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.uuid "uuid", default: -> { "uuid_generate_v4()" }, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uuid"], name: "index_users_on_uuid", unique: true
   end
 
 end
