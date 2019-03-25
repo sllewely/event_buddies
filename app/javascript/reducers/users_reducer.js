c617bd3d459ddc9f3c60299cfca0b618eb404c0e
Happler <harry.appler@gmail.com>
2019-03-24 20:13:58 -0400

    fixed defailt in users reducer

--- app/javascript/reducers/users_reducer.js
+++ app/javascript/reducers/users_reducer.js
@@ -1,7 +1,7 @@
 import { merge } from "lodash";
 import { RECEIVE_USERS } from "../actions/user_actions";
 
-const UsersReducer = (state, action) => {
+const UsersReducer = (state = [], action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_USERS:
