592585e056930708fa1a729cd30fdc4555b237ee
Sarah Llewelyn <se.llewelyn@gmail.com>
2019-03-31 16:26:45 -0400

    fix sql for hosts

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ApplicationRecord
   has_many :users, through: :user_event_responses
 
   def hosts
-    users.joins(:user_event_responses).where(user_event_responses: { host: true })
+    users.where(user_event_responses: { host: true })
   end
 
 end
