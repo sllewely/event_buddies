9c4f509d5a5b27c224aa6b3f0ffff0720901e3c9
Sarah Llewelyn <se.llewelyn@gmail.com>
2019-04-15 14:29:40 -0400

    tests are passing

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ApplicationRecord
   has_many :user_event_responses
   has_many :users, through: :user_event_responses
 
+  # Get the hosts for the event
   def hosts
     users.where(user_event_responses: { host: true })
   end
--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -31,14 +31,19 @@ RSpec.describe 'Events API', type: :request do
     end
 
     it 'gets all of the events I have subscribed to' do
-      event_a = build(:event, name: 'Mini Mansions', date_time: '2018-7-23-21.5')
-      event_b = build(:event, name: 'Queens of the Stone Age', date_time: '2018-7-25-21.5')
-      event_c = build(:event, name: 'Queens of the Stone Age', date_time: '2018-7-25-21.5')
-      build(:user_event_response)
+      event_a = create(:event, name: 'Mini Mansions', date_time: '2018-7-23-21.5')
+      event_b = create(:event, name: 'Queens of the Stone Age', date_time: '2018-7-25-21.5')
+      # I am not invited to event_c, so it is not in the response
+      event_c = create(:event, name: 'Queen', date_time: '2018-7-25-21.5')
+      create(:user_event_response, user: user, event: event_a)
+      create(:user_event_response, user: user, event: event_b)
 
       get '/api/v1/events', as: :json
       result = JSON.parse(response.body)
 
+      expect(result.size).to eq(2)
+      expect(result.any? { |r| r['name'] == 'Mini Mansions' }).to be(true)
+      expect(result.any? { |r| r['name'] == 'Queens of the Stone Age' }).to be(true)
     end
 
     it 'gets all of the events I have created' do
@@ -54,7 +59,19 @@ RSpec.describe 'Events API', type: :request do
     end
 
     it 'gets all of the events I am subscribed to or am the host of' do
-      # TODO
+      event_a = create(:event, name: 'Mini Mansions', date_time: '2018-7-23-21.5')
+      event_b = create(:event, name: 'Queens of the Stone Age', date_time: '2018-7-25-21.5')
+      # I am not invited to event_c, so it is not in the response
+      event_c = create(:event, name: 'Queen', date_time: '2018-7-25-21.5')
+      create(:user_event_response, user: user, event: event_a, host: true)
+      create(:user_event_response, user: user, event: event_b)
+
+      get '/api/v1/events', as: :json
+      result = JSON.parse(response.body)
+
+      expect(result.size).to eq(2)
+      expect(result.any? { |r| r['name'] == 'Mini Mansions' }).to be(true)
+      expect(result.any? { |r| r['name'] == 'Queens of the Stone Age' }).to be(true)
     end
   end
 end
