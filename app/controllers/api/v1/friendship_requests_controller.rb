2a2ab6b70ade8b90e2e823823379009f5e2936d2
Sarah Llewelyn <se.llewelyn@gmail.com>
2019-04-15 17:06:59 -0400

    routes for friendship requests with skipped tests

--- app/controllers/api/v1/friendship_requests_controller.rb
+++ app/controllers/api/v1/friendship_requests_controller.rb
@@ -1,18 +1,18 @@
 class API::V1::FriendshipRequestsController < API::V1::APIController
 
   def index
-
+    json_response('not implemented')
   end
 
   def create
-
+    json_response('not implemented')
   end
 
   def confirm
-
+    json_response('not implemented')
   end
 
   def reject
-
+    json_response('not implemented')
   end
 end
--- spec/requests/friendship_requests_spec.rb
+++ spec/requests/friendship_requests_spec.rb
@@ -4,14 +4,31 @@ RSpec.describe 'Friendship Requests Responses API', type: :request do
   let(:user) { create :user }
   let(:user2) { create :user, first_name: 'Jim' }
 
+  before do
+    sign_in user
+  end
+
   describe 'POST /friendship_requests/' do
-
-    before do
-      sign_in user
-    end
-
-    it 'creates a new friendship request with the given user' do
+    xit 'creates a new friendship request with the given user' do
       post "/api/v1/friendship_requests", params: { format: :json }
+    end
+  end
+
+  describe 'GET /friendship_requests/' do
+    xit 'gets all pending friendship requests' do
+      get '/api/v1/friendship_requests', params: { format: :json }
+    end
+  end
+
+  describe 'POST /friendship_requests/:uuid/confrim' do
+    xit 'accepts the given friendship request and creates a friendship relation' do
+      post "/api/v1/friendship_requests/#{user2.id}/confirm"
+    end
+  end
+
+  describe 'POST /friendship_requests/:uuid/reject' do
+    xit 'rejects the given friendship request' do
+      post "/api/v1/friendship_requests/#{user2.id}/reject"
     end
   end
 end
