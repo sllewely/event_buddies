88ae4c0d329cf0d6ab82bc4a63ba60881978d0e8
Sarah Llewelyn <se.llewelyn@gmail.com>
2019-06-26 18:27:44 -0400

    timestamp in migration

--- db/migrate/20190512201011_add_time_stamp_to_friendship_tables.rb
+++ db/migrate/20190512201011_add_time_stamp_to_friendship_tables.rb
@@ -1,9 +1,6 @@
 class AddTimeStampToFriendshipTables < ActiveRecord::Migration[5.2]
   def change
-    add_column :friendship_requests, :created_at, :datetime, null: false
-    add_column :friendship_requests, :updated_at, :datetime, null: false
-
-    add_column :friendships, :created_at, :datetime, null: false
-    add_column :friendships, :updated_at, :datetime, null: false
+    add_timestamps :friendship_requests, null: false
+    add_timestamps :friendships, null: false
   end
 end
