cffa7ef052bbff54672101326e076d8d81995a3f
Happler <harry.appler@gmail.com>
2019-09-22 21:56:02 -0400

    use protected route

--- app/javascript/components/app.jsx
+++ app/javascript/components/app.jsx
@@ -7,27 +7,27 @@ import { AuthRoute, ConRoute } from "../utils/route_utils";
 
 // TODO: I am doing this now to be able to work on protected routes before they can be accessed,
 // TODO: once they're ready comment in the bottom one and delete the top
-const App = () => (
-  <main>
-    <Switch>
-      <Router />
-    </Switch>
-  </main>
-);
-
 // const App = () => (
 //   <main>
 //     <Switch>
-//       <AuthRoute exact path="/login" component={LoginForm} />
-//       <AuthRoute exact path="/signup" component={SignupForm} />
-//       <ConRoute
-//         path="/"
-//         conditional={loggedStatus => loggedStatus}
-//         FalseComp={LoginForm}
-//         TrueComp={Router}
-//       />
+//       <Router />
 //     </Switch>
 //   </main>
 // );
+
+const App = () => (
+  <main>
+    <Switch>
+      <AuthRoute exact path="/login" component={LoginForm} />
+      <AuthRoute exact path="/signup" component={SignupForm} />
+      <ConRoute
+        path="/"
+        conditional={loggedStatus => loggedStatus}
+        FalseComp={LoginForm}
+        TrueComp={Router}
+      />
+    </Switch>
+  </main>
+);
 
 export default App;
