65af07153532cd102929030fbb0356a631268af5
Sarah Llewelyn <se.llewelyn@gmail.com>
2019-09-23 19:51:00 -0400

    permitted params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,18 @@
 class RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
+  before_action :configure_permitted_parameters
+
   def create
     build_resource(sign_up_params)
 
     resource.save
     render_resource(resource)
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password])
   end
 end
