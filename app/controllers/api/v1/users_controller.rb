e4d483e7efec214589ac5874a9a85f8ce4027691
Sarah Llewelyn <se.llewelyn@gmail.com>
2019-09-23 20:25:45 -0400

    user serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,6 @@
 class API::V1::UsersController < API::V1::APIController
 
   def show
-    selected_fields = User::PUBLIC_FIELDS
-    selected_fields += [:email] if params[:id] == current_user.id
-
-    json_response(User.select(selected_fields).find(params[:id]))
+    json_response(User.find(params[:id]))
   end
 end
