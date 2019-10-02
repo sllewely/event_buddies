a6d055035428dcb063e54d0bf55cce15da34b481
Sarah Llewelyn <se.llewelyn@gmail.com>
2019-10-01 20:51:18 -0400

    get all users

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
         end
       end
 
-      resources :users, only: [:show]
+      resources :users, only: [:show, :index]
 
       resources :friendships, only: [:index]
     end
