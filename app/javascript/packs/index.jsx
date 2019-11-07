c314706efa247513039d371926df8aae2cc2d07b
Sarah Llewelyn <se.llewelyn@gmail.com>
2019-11-07 16:52:33 -0500

    drop font loader

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -4,12 +4,6 @@ import configureStore from "../store/store";
 import Root from "../components/root";
 import WebFont from 'webfontloader';
 
-WebFont.load({
-  google: {
-    families: ['Titillium Web:300,400,700', 'sans-serif']
-  }
-});
-
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
   const token = document.getElementsByName("csrf-token")[0].content;
