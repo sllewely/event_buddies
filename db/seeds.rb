fdee20124bb25e0b6ff3bcb269366065411be9c3
Sarah Llewelyn <se.llewelyn@gmail.com>
2019-11-06 21:21:03 -0500

    fix faker warnings

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@ Event.destroy_all
     name: Faker::TvShows::SiliconValley.invention,
     location: Faker::Space.galaxy,
     description:Faker::Books::Lovecraft.paragraph,
-    event_link: Faker::Internet.url('example.com'),
-    date_time:Faker::Time.forward(80, :evening),
+    event_link: Faker::Internet.url(host: 'example.com'),
+    date_time:Faker::Time.forward(days: 80, period: :evening),
   )
   UserEventResponse.create(
     user:User.first,
