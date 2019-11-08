d0d7bf4532d005e638b1f7a4abf1d842061911dd
Happler <harry.appler@gmail.com>
2019-11-08 01:22:55 -0500

    display friend names

--- app/javascript/reducers/session_reducer.js
+++ app/javascript/reducers/session_reducer.js
@@ -1,19 +1,25 @@
 import { merge } from "lodash";
 import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
 import { RECEIVE_JWT_TOKEN } from "../actions/session_actions";
+import { RECEIVE_FRIENDS } from "../actions/user_actions";
 
 const SessionReducer = (state = { currentUser: null }, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
+      const currentUser = action.payload.receivedUser;
+      currentUser["friend_ids"] = [];
       return merge({}, state, {
-        currentUser: action.payload.receivedUser,
+        currentUser,
         jwtToken: action.payload.token
       });
     case RECEIVE_JWT_TOKEN:
       return merge({}, state, {
         jwtToken: action.payload
       });
+    case RECEIVE_FRIENDS:
+      const friend_ids = action.payload.map(friend => friend["id"]);
+      return merge({}, state, { currentUser: { friend_ids } });
     default:
       return state;
   }
