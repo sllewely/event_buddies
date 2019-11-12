ae24db344151660cbf2105fdb14e7f76c8c2e0f9
Sarah Llewelyn <se.llewelyn@gmail.com>
2019-11-11 21:15:48 -0500

    carry over main styling

--- app/javascript/components/friends/friends_page.jsx
+++ app/javascript/components/friends/friends_page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 class FriendsPage extends React.Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <h1>Friends!</h1>
       </div>
     );
