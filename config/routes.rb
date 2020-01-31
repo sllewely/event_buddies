6dfa650244903a79b618af1ce270ca46abe2fe74
Bob Umerkulov <b.umerkulov@gmail.com>
2020-01-30 19:16:03 -0500

    Passwords controller responds to JSON

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users,
     path: '',
     path_names: { sign_in: 'login', sign_out: 'logout', registration: 'signup' },
-    controllers: { sessions: 'sessions', registrations: 'registrations' }
+    controllers: { sessions: 'sessions', registrations: 'registrations', passwords: 'passwords' }
 
   root to: 'home#index'
 
