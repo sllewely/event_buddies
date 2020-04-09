801e9aead80a7d3265eca37b008e66ed3ea59276
Saaaaaage <sage.browning@gmail.com>
2020-04-09 14:43:06 -0600

    profile pic thumbnails added to GET API response for users

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,13 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name
+  include Rails.application.routes.url_helpers
+  
+  attributes :id, :first_name, :last_name, :profile_thumbnail_url
   attribute :email, if: :is_current_user?
+
+  def profile_thumbnail_url
+    variant = object.profile_pic.variant(resize: "100x100")
+    return rails_representation_url(variant, only_path: true)
+  end
 
   def is_current_user?
     current_user && object.id == current_user.id
